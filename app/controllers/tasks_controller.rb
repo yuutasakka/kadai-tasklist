4028145f3313251a058b35e8ccacf25ec5b22e0d
atyuatyuyu1025@gmail.com <y.atyuadg.77@gmail.com>
2017-11-19 05:44:03 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy] 
+  before_action :require_user_logged_in, only: [:index, :show]
 
   def index
     @tasks = Task.all.page(params[:page]).per(3)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: 'tasks#index'
   
-  resources :tasks
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'sighup', to: 'users#new'
+  
+  resources :tasks, only: [:index, :show, :new, :create]
 end
