3c1dfdf79dee6b0605e11d801e08690534aa8feb
atyuatyuyu1025@gmail.com <y.atyuadg.77@gmail.com>
2017-11-19 13:52:51 +0000

    task

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   root to: 'tasks#index'
-  
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
   
   get 'sighup', to: 'users#new'
   
-  resources :tasks, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create]
 end
