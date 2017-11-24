f55296ab5c7c239c88f384718948185e27cb546d
atyuatyuyu1025@gmail.com <y.atyuadg.77@gmail.com>
2017-11-24 01:04:40 +0000

    goodmorning

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy] 
   before_action :require_user_logged_in, only: [:index, :show, :create, :destroy]
-  before_action :correct_user, only: [:edit, :destroy]
+  before_action :correct_user, only: [:show, :update, :edit, :destroy]
 
   def index
      if logged_in?
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   
+  validates :status, presence: true, length: { maximum: 10 }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
 end
